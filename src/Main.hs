1477b69116c515ee51400b8f34441d056732b0f5
fabian <f.markert87@googlemail.com>
2012-08-06 18:59:23 +0200

    project is buildable the first time :D

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,8 @@
 module Main where
 
 import Data.Conduit
-import Data.Conduit.Binary
-import Data.Char
 import Data.Conduit.Network as Nw
 import Data.Conduit.List
-import Control.Monad
 import Data.Conduit.Text as Ct
 import Data.Conduit.Binary as Cb
 
@@ -16,8 +13,9 @@ main = runTCPServer (ServerSettings 4000 HostAny) server
      
 server::Nw.Application IO 
 server src sink = do
-        runResourceT ( src $$ (Cb.sinkFile "something.txt"))
-        --Ct.decode Ct.utf8 =$ Ct.encode Ct.utf8 =$ 
-        print "fertig"
+        x<- src $$ consume                    
+        runResourceT $ sourceList x $$ Ct.decode Ct.utf8 =$ Ct.encode Ct.utf8 =$ Cb.sinkFile "something.txt"
+        
+        --print "fertig"
             
        
