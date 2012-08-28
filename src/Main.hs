a00f9d699e315763d9d200139f69e06f0e84829f
fabian <f.markert87@googlemail.com>
2012-08-28 18:47:01 +0200

    try to integrate pandoc

--- src/Main.hs
+++ src/Main.hs
@@ -6,15 +6,17 @@ import Data.Conduit.Network as Nw
 import Data.Conduit.List
 import Data.Conduit.Text as Ct
 import Data.Conduit.Binary as Cb
+import Text.Pandoc.Readers.Markdown as Prm
+import Text.Pandoc.Writers.HTML as Pwh
 
 main::IO()
 main = runTCPServer (ServerSettings 4000 HostAny) server
      
      
 server::Nw.Application IO 
-server src sink = do
+server src _ = do
         x<- src $$ consume                    
-        runResourceT $ sourceList x $$ Ct.decode Ct.utf8 =$ Ct.encode Ct.utf8 =$ Cb.sinkFile "something.txt"
+        runResourceT $ sourceList x $$ Ct.decode Ct.utf8 =$ Prm.readMarkdown =$ Pwh.writeHtmlString =$ Ct.encode Ct.utf8 =$ Cb.sinkFile "something.txt"
         
         --print "fertig"
             
